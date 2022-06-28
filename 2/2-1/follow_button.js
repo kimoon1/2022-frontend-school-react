4fe8330685d9b74d5e1a96bde09b662022e1f188
이기문 <kimoon212@gmail.com>
2022-06-28 13:54:06 +0900

    2-2 실습 코드 입력

--- 2/2-1/follow_button.js
+++ 2/2-1/follow_button.js
@@ -1,5 +1,39 @@
 // React Component!
 // Follow <-> following
 function FollowButton() {
-  return React.createElement("div", {}, "Follow");
+  const [following, setFollowing] = React.useState(false);
+
+  const commonBtnStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    top: "12px",
+    right: "16px",
+    width: "100px",
+    height: "36px",
+    borderRadius: "9999px",
+    fontWeight: "bold",
+  };
+
+  const followBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "black",
+    color: "white",
+  };
+
+  const followingBtnStyle = {
+    ...commonBtnStyle,
+    backgroundColor: "white",
+    color: "black",
+    border: "1px solid #cfd9de",
+  };
+
+  return React.createElement("div", {
+    onClick: () => setFollowing(!following),
+    style: following ? followingBtnStyle : followBtnStyle,
+  }, following ? "following" : "Follow");
 }
+
+const domContainer = document.querySelector('#follow_button_container');
+ReactDOM.render(React.createElement(FollowButton), domContainer);
+
