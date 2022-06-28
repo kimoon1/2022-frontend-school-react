106f98d2294df0b6f4d1f2d81f1f4b01ffc2abe0
이기문 <kimoon212@gmail.com>
2022-06-28 15:12:53 +0900

    리액트 프로젝스 수동 생성 실습

---
+++ 2/withoutCRA/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FollowButton from './components/FollowButton.js'
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<FollowButton />, domContainer);
