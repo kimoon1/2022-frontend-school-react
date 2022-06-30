24017662ccd90b9cd188509a8e81865a1849da77
이기문 <kimoon212@gmail.com>
2022-06-30 17:57:06 +0900

    윈시형 useState 실습

--- 2/my-app/src/components/Counter.js
+++ 2/my-app/src/components/Counter.js
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
 
 function Counter() {
+  //값을 변경할때 쓰는 함수
   const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
+
+  //+, - , *
+  const operators = ['+', '-', '*'];
+  const [operator, setOperator] = useState(operators[0])
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <br />Counter: {count}
+      <button onClick={() => {
+        let result;
+        if (operator === '+') result = count + 1;
+        if (operator === '-') result = count - 1;
+        if (operator === '*') result = count * 1;
+        setCount(result);
+      }}>{operator}1</button>
+      <button onClick={() => setShow(!show)}>Show and Hide</button>
+      <button onClick={() => {
+        const idx = Math.floor(Math.random() * operators.length);
+        setOperator(operators[idx]);
+      }}>Change Operator</button>
+      <br />
+      {show && `Counter: ${count}`}
     </div>
   );
 }
