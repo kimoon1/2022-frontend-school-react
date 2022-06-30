684ecd91532c90d1515e53d3d4ed9ef02c9b6232
이기문 <kimoon212@gmail.com>
2022-06-30 18:29:20 +0900

    비동기 useState 테스트

--- 2/my-app/src/App.js
+++ 2/my-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
+import Counter2 from './components/Counter2'
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Counter2 />
     </div>
   );
 }
