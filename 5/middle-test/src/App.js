bf7446a75898f58895f08fdc2e61369a8a3e2ac2
이기문 <kimoon212@gmail.com>
2022-07-01 17:25:16 +0900

    중간 미션 생성

---
+++ 5/middle-test/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Cardscomponents from './component/cardscomponent'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Cardscomponents />
+    </div>
+  );
+}
+
+export default App;
