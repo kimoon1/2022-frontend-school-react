44aad6a2350d016b7c51e45cc783b07cc6ae28c1
이기문 <kimoon212@gmail.com>
2022-07-01 16:23:06 +0900

    클래스 컴포넌트를 함수형으로 변환 실습

--- 2/my-app/src/App.js
+++ 2/my-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Clock from './components/Clock';
+import FunctionClock from './components/FunctionClock';
 
 function App() {
   return (
     <div className="App">
-      <Clock />
+      <FunctionClock />
     </div>
   );
 }
