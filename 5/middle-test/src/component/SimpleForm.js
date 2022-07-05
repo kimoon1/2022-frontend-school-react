85fdfe0e83cc34ee3ded46046ef5631e13997c0e
이기문 <kimoon212@gmail.com>
2022-07-05 17:09:01 +0900

    다중입력 폼 다루기

--- 5/middle-test/src/component/SimpleForm.js
+++ 5/middle-test/src/component/SimpleForm.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 
 export default function SimpleForm() {
-  const [nickname, setNickName] = useState('');
+  // const [nickname, setNickName] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const [userInputs, setUserInputs] = useState({
+    nickname: '',
+    password: '',
+  });
   const handleChange = (e) => {
-    setNickName(e.target.value);
+    setUserInputs({ ...userInputs, [e.target.name]: e.target.value });
+    // if (e.target.name === 'nickname') return setNickName(e.target.value);
+    // return setUserInputs(e.target.value);
+
+
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // 서버 요청 + a
-    alert(nickname);
+    alert(`nickname: ${userInputs.nickname} password: ${userInputs.password}`);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label>닉네임 :</label>
-      <input type='text' name='nickname' onChange={handleChange} value={nickname}></input>
+      <input type='text' name='nickname' onChange={handleChange} value={userInputs.nickname}></input>
+      <br />
+      <label>패스워드 :</label>
+      <input type='text' name='password' onChange={handleChange} value={userInputs.password}></input>
       <input type='submit' value='제출' />
     </form>
   );
