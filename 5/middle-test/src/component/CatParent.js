c4f0d83cd7e3575a2a8a576f76c28958c309f819
이기문 <kimoon212@gmail.com>
2022-07-07 14:18:23 +0900

    콜백 ref 실습

--- 5/middle-test/src/component/CatParent.js
+++ 5/middle-test/src/component/CatParent.js
@@ -1,18 +1,32 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Cat from "./Cat";
 
 export default function CatParent() {
+  const [height, setHeight] = useState(0);
   console.log("부모 컴포넌트 CatParent");
-  const catRef = useRef();
+  // const catRef = useRef();
+  const catCallbackRef = (node) => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  }
 
-  console.log(catRef);
+  // console.log(catRef);
+
+  const ageRef = useRef(1);
+
+  // console.log(ageRef.current);
 
   return (
     <div>
       <h4> 고양이가 세상을 구한다 ️</h4>
       <div>
-        <Cat ref={catRef} />
-        <button onClick={() => alert(catRef.current.height)}>고양이의 크기를 알고싶어</button>
+        <Cat a={'a'} ref={catCallbackRef} />
+        <h3>나이: {ageRef.current}살</h3>
+        <h4>키: {height}</h4>
+        {/* <button onClick={() => alert(catRef.current.height)}>고양이의 크기를 알고싶어</button> */}
+        <button onClick={() => ageRef.current = ageRef.current + 1}>새해</button>
+        {/* <button onClick={() => setState(state + 1)}>테스트</button> */}
       </div>
     </div>
   );
